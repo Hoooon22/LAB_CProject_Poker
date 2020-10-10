8fee2b1999c20916ea344c57c6a84bca195afaef
Hoooon22 <mommo990305@gmail.com>
2020-10-10 21:59:05 +0900

    7 turn

--- HRILAB_C_Project/Game.c
+++ HRILAB_C_Project/Game.c
@@ -73,4 +73,85 @@ void start(struct Deck *deck, struct Player *p, struct Player c[])
 
 	printf("\n□□□□□□□ Turn %d Finish □□□□□□□\n", turn);
 
+	// game(3) - give one card
+	printf("\n□□□□□□□□□ Turn %d □□□□□□□□□\n", ++turn);
+	printf("Giving card...\n");
+	getCard(p, deck);
+	getCard(&c[0], deck);
+	getCard(&c[1], deck);
+	getCard(&c[2], deck);
+
+	for (int i = 0; i < 3; i++)
+	{
+		openCard(&c[i], 4, 1);
+	}
+
+	printf("==Player Card list==\n"); // player
+	viewCard(p);
+	printf("==Computer 1 Card list==\n"); // Computer 1
+	viewCard(&c[0]);
+	printf("==Computer 2 Card list==\n"); // Computer 2
+	viewCard(&c[1]);
+	printf("==Computer 3 Card list==\n"); // Computer 3
+	viewCard(&c[2]);
+
+	printf("==Turn %d Betting!==\n", turn); // turn 1 Betting
+	bettingPlayer(p);
+
+	printf("\n□□□□□□□ Turn %d Finish □□□□□□□\n", turn);
+
+	// game(4) - give one card
+	printf("\n□□□□□□□□□ Turn %d □□□□□□□□□\n", ++turn);
+	printf("Giving card...\n");
+	getCard(p, deck);
+	getCard(&c[0], deck);
+	getCard(&c[1], deck);
+	getCard(&c[2], deck);
+
+	for (int i = 0; i < 3; i++)
+	{
+		openCard(&c[i], 5, 1);
+	}
+
+	printf("==Player Card list==\n"); // player
+	viewCard(p);
+	printf("==Computer 1 Card list==\n"); // Computer 1
+	viewCard(&c[0]);
+	printf("==Computer 2 Card list==\n"); // Computer 2
+	viewCard(&c[1]);
+	printf("==Computer 3 Card list==\n"); // Computer 3
+	viewCard(&c[2]);
+
+	printf("==Turn %d Betting!==\n", turn); // turn 1 Betting
+	bettingPlayer(p);
+
+	printf("\n□□□□□□□ Turn %d Finish □□□□□□□\n", turn);
+
+
+	// game(5) - give hidden card
+	printf("\n□□□□□□□□□ Turn %d □□□□□□□□□\n", ++turn);
+	printf("Giving card...\n");
+	getCard(p, deck);
+	getCard(&c[0], deck);
+	getCard(&c[1], deck);
+	getCard(&c[2], deck);
+
+	// hidden -> no open
+
+	printf("==Player Card list==\n"); // player
+	viewCard(p);
+	printf("==Computer 1 Card list==\n"); // Computer 1
+	viewCard(&c[0]);
+	printf("==Computer 2 Card list==\n"); // Computer 2
+	viewCard(&c[1]);
+	printf("==Computer 3 Card list==\n"); // Computer 3
+	viewCard(&c[2]);
+
+	printf("==Turn %d Betting!==\n", turn); // turn 1 Betting
+	bettingPlayer(p);
+
+	printf("\n□□□□□□□ Turn %d Finish □□□□□□□\n", turn);
+
+	// 이제 승패 결정
+
 }
